03b4a1158713a48c9b3e6486de507049de1db49d
xin <xin@wanplus.com>
2018-05-17 18:47:00 +0800

    误将数据库配置提交，修改回来了

--- application/config/database.php
+++ application/config/database.php
@@ -75,12 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'mysql',
-//	'username' => 'est_esports_chain',
-	'username' => 'root',
-//	'password' => 'est@super#NB',
-	'password' => 'root',
-//	'database' => 'est_esports_chain',
+	'username' => 'est_esports_chain',
+	'password' => 'est@super#NB',
+	'database' => 'est_esports_chain',
 	'database' => 'esportchain',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
