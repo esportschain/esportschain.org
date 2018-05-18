b5ceb81647219eb0f86672d14251c19fd5473a47
xin <xin@wanplus.com>
2018-05-18 13:45:56 +0800

    change session to cookie postparam check

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,8 +8,8 @@ class Admin extends CI_Controller    {
      */
     public function __islogin()
     {
-        $this->load->library('session');
-        if($this->session->is_login){
+        $this->load->helper('cookie');
+        if(get_cookie('is_login')){
             return true;
         }else{
             return false;
@@ -32,8 +32,8 @@ class Admin extends CI_Controller    {
      */
     public function login()
     {
-        $this->load->library('session');
-        if($this->session->is_login){
+        $this->load->helper('cookie');
+        if(get_cookie('is_login')){
             $this->load->helper('url');
             redirect('c=admin&m=index','location');
         }else{
@@ -46,16 +46,30 @@ class Admin extends CI_Controller    {
     }
 
     /**
+     * 登陆页面
+     */
+    public function loginout()
+    {
+        $this->load->helper('cookie');
+        delete_cookie('is_login');
+        echo '<script>alert("已退出！");window.location.href="/index.php?c=admin&m=login";</script>';
+    }
+
+    /**
      * 登陆验证
      */
     public function checkLogin()
     {
-        $name = 'wanplus_esc';
-        $pass = 'esc_wanplus';
         $post = $this->input->post();
-        if($post['username'] == $name && $post['password'] == $pass){
-            $this->load->library('session');
-            $this->session->set_userdata('is_login', true);
+        if(!$post['username'] || !$post['password']){
+            echo '<script>alert("请输入用户名和密码！");window.location.href="/index.php?c=admin&m=login";</script>';
+        }
+        $this->config->load('user', TRUE);
+        $configname = $this->config->item('username',"user");
+        $configpass = $this->config->item('password',"user");
+        if($post['username'] == $configname && md5($post['password']) == $configpass){
+            $this->load->helper('cookie');
+            set_cookie('is_login','1',86400);
             echo '<script>alert("登陆成功！");window.location.href="/index.php?c=admin&m=index";</script>';
         }else{
             echo '<script>alert("登陆失败！");window.location.href="/index.php?c=admin&m=login";</script>';
@@ -97,13 +111,22 @@ class Admin extends CI_Controller    {
         date_default_timezone_set('PRC');
         $this->load->database();
         if($this->input->method() == 'post') {
+            // 获取数据
             $post = $this->input->post();
+            // 标题和链接都不能为空
+            if(!$post['cn_title']||!$post['en_title']||!$post['kr_title']||!$post['cn_url']||!$post['en_url']||!$post['kr_url']){
+                echo '<script>alert("标题和链接都不能为空！");window.location.href="/index.php?c=admin&m=editnews&newsid='.$post['id'].'";</script>';
+            }
+            // 发布时间不能为0 格式必须正确
             $post['publish_time'] = strtotime($post['publish_time']);
+            if(!$post['publish_time']){
+                echo '<script>alert("请按照规定格式填写发布时间！");window.location.href="/index.php?c=admin&m=editnews&newsid='.$post['id'].'";</script>';
+            }
             $post['add_time'] = strtotime($post['add_time']);
+            // update数据库
             $this->db->replace('est_news', $post);
             $this->load->helper('url');
             echo '<script>alert("修改成功！");window.location.href="/index.php?c=admin&m=news";</script>';
-//            redirect('/index.php?c=admin&m=news','location');
         }else{
             $newsid = $this->input->get('newsid');
             $this->db->from('est_news');
@@ -130,13 +153,21 @@ class Admin extends CI_Controller    {
         date_default_timezone_set('PRC');
         if($this->input->method() == 'post') {
             $post = $this->input->post();
+            // 标题和链接都不能为空
+            if(!$post['cn_title']||!$post['en_title']||!$post['kr_title']||!$post['cn_url']||!$post['en_url']||!$post['kr_url']){
+                echo '<script>alert("标题和链接都不能为空！");window.location.href="/index.php?c=admin&m=addnews";</script>';
+            }
+            // 发布时间不能为0 格式必须正确
             $post['publish_time'] = strtotime($post['publish_time']);
+            if(!$post['publish_time']){
+                echo '<script>alert("请按照规定格式填写发布时间！");window.location.href="/index.php?c=admin&m=addnews";</script>';
+            }
             $post['add_time'] = time();
+            // insert 数据库
             $this->load->database();
             $this->db->insert('est_news', $post);
             $this->load->helper('url');
             echo '<script>alert("添加成功！");window.location.href="/index.php?c=admin&m=news";</script>';
-//            redirect('/index.php?c=admin&m=news','location');
         }else{
             $csrf = array(
                 'csrf_name' => $this->security->get_csrf_token_name(),
